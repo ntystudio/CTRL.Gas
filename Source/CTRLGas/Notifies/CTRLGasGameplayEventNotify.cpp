6766410ae0afaee1d887cb71d3002ec7b8bb0e57
Tim Oxley <secoif@gmail.com>
2025-04-30 13:00:11 -0400

    Fix non-editor build.

--- Source/CTRLGas/Notifies/CTRLGasGameplayEventNotify.cpp
+++ Source/CTRLGas/Notifies/CTRLGasGameplayEventNotify.cpp
@@ -11,8 +11,6 @@
 
 #include "Misc/UObjectToken.h"
 
-#if WITH_EDITOR
-
 void UCTRLGasGameplayEventNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, FAnimNotifyEventReference const& EventReference)
 {
 	Super::Notify(MeshComp, Animation, EventReference);
@@ -20,6 +18,7 @@ void UCTRLGasGameplayEventNotify::Notify(USkeletalMeshComponent* MeshComp, UAnim
 	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, EventData.EventTag, EventData);
 }
 
+#if WITH_EDITOR
 void UCTRLGasGameplayEventNotify::ValidateAssociatedAssets()
 {
 	static FName const NAME_AssetCheck("AssetCheck");
